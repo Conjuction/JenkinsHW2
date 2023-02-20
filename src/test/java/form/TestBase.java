b9c9905b7ed49ffed8816ea9ffd7f580ebeb57df
Conjuction <suhinin-d@mail.ru>
2023-02-20 17:34:00 +0500

    added choice browser,browserVersion,browserSize

--- src/test/java/form/TestBase.java
+++ src/test/java/form/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         Configuration.browser = System.getProperty("browser", "chrome");;
         Configuration.browserVersion = System.getProperty("browserVersion", "100");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
